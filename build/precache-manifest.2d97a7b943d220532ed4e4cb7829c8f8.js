55d0ef28112a71b572fb62375e942b6b7cb9681c
edwardsifi <edwardsifi0@gmail.com>
2020-07-31 09:19:50 -0600

    prototipo de google cloud

---
+++ build/precache-manifest.2d97a7b943d220532ed4e4cb7829c8f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8081f7e85c7e90196357fec2b90787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d31ef0f44c4d1e8616",
+    "url": "/static/css/2.0dc910f2.chunk.css"
+  },
+  {
+    "revision": "b374627230e4d30cf42a",
+    "url": "/static/css/main.5e9feb98.chunk.css"
+  },
+  {
+    "revision": "d1d31ef0f44c4d1e8616",
+    "url": "/static/js/2.17b9b489.chunk.js"
+  },
+  {
+    "revision": "b374627230e4d30cf42a",
+    "url": "/static/js/main.9355d053.chunk.js"
+  },
+  {
+    "revision": "46f6293c3ca389710bde",
+    "url": "/static/js/runtime~main.cd912dc6.js"
+  },
+  {
+    "revision": "7672211f03abe474a4ae321dc7022a73",
+    "url": "/static/media/pimp.7672211f.svg"
+  },
+  {
+    "revision": "414701bff487824dd2071ca288efaae8",
+    "url": "/static/media/ubimed-logo.414701bf.png"
+  }
+]);
