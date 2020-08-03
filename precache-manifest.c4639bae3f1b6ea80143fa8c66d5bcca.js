1d19a5c72971424d616a9ce5a1ddefad5c53c454
edwardsifi <edwardsifi0@gmail.com>
2020-08-03 13:26:10 -0600

    resubiendo el proyecto

---
+++ precache-manifest.c4639bae3f1b6ea80143fa8c66d5bcca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a77103b3d34d943c9f39026bf6aef6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5bee4f7457ef1e949c0",
+    "url": "/static/css/2.0dc910f2.chunk.css"
+  },
+  {
+    "revision": "bacbf7b5661b2568eab4",
+    "url": "/static/css/main.5e9feb98.chunk.css"
+  },
+  {
+    "revision": "a5bee4f7457ef1e949c0",
+    "url": "/static/js/2.960b5132.chunk.js"
+  },
+  {
+    "revision": "bacbf7b5661b2568eab4",
+    "url": "/static/js/main.a010dcc6.chunk.js"
+  },
+  {
+    "revision": "46f6293c3ca389710bde",
+    "url": "/static/js/runtime~main.cd912dc6.js"
+  },
+  {
+    "revision": "7672211f03abe474a4ae321dc7022a73",
+    "url": "/static/media/pimp.7672211f.svg"
+  },
+  {
+    "revision": "414701bff487824dd2071ca288efaae8",
+    "url": "/static/media/ubimed-logo.414701bf.png"
+  }
+]);
